55d5be6ade725afc0e118af153fcf250aa88a6a0
wallyson <wallysom2@gmail.com>
2023-04-24 10:58:19 -0300

    fix navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,55 +3,43 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Navbar: React.FC = () => {
-	return (
-		<NavbarWrapper>
-			<Link to="/">
-				<NavbarLink>Check</NavbarLink>
-			</Link>
-			<Link to="/blacklist">
-				<NavbarLinkBlacklist>Blacklist</NavbarLinkBlacklist>
-			</Link>
-			<Link to="/whitelist">
-				<NavbarLinkWhitelist>Whitelist</NavbarLinkWhitelist>
-			</Link>
-		</NavbarWrapper>
-	);
+  return (
+    <NavbarWrapper>
+      <Link to="/">
+        <NavbarLink>Check</NavbarLink>
+      </Link>
+      <Link to="/blacklist">
+        <NavbarLink>Blacklist</NavbarLink>
+      </Link>
+      <Link to="/whitelist">
+        <NavbarLink>Whitelist</NavbarLink>
+      </Link>
+    </NavbarWrapper>
+  );
 };
 
 export default Navbar;
+
+
 
 const NavbarWrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
   background-color: #fff;
-  padding: 40px 80px;
+  padding: 20px;
   box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
-
 `;
 
 const NavbarLink = styled.a`
-  font-family: 'Poppins', sans-serif;
-  font-size: 1.6rem;
-  color: #2d3748;
+  font-size: 18px;
+  color: #555;
   text-decoration: none;
-  margin-right: 1rem;
-  padding: 0.5rem 1rem;
+  margin-right: 20px;
   transition: all 0.3s;
-
   &:hover {
     color: #0c7cd5;
   }
 `;
 
-const NavbarLinkBlacklist = styled(NavbarLink)`
-  &:hover {
-    color: #aaa;
-  }
-`;
-
-const NavbarLinkWhitelist = styled(NavbarLink)`
-  &:hover {
-    color: darkgreen;
-  }
-`;
+export { NavbarWrapper, NavbarLink };
